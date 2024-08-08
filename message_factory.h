95f28c12412ffeb177ce383c0365cae4144976c7
xvasil10 <xvasil10@stud.fit.vutbr.cz>
2024-08-08 22:54:04 +0200

    Fix wrong file name in include guard

--- message_factory.h
+++ message_factory.h
@@ -1,5 +1,5 @@
-#ifndef ABSTACT_FACTORY_H
-#define ABSTACT_FACTORY_H
+#ifndef MESSAGE_FACTORY_H
+#define MESSAGE_FACTORY_H
 
 #include <string>
 
@@ -10,4 +10,4 @@ class MessageFactory
 public:
   virtual Message *create(const std::string &message) = 0;
 };
-#endif // ABSTACT_FACTORY_H
+#endif // MESSAGE_FACTORY_H
